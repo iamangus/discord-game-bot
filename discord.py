35decad65dcf2de43d2dd5388db6a021a632197f
iamangus <turnbull.angus@gmail.com>
2022-01-19 11:30:09 -0600

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,1 +1,14 @@
+import discord
+import logging
 
+from discord import commands
+
+logging.basicConfig(level=logging.DEBUG)
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready()
+    print('Bot is ready')
+
+client.run('API_KEY')
