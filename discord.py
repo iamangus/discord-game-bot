49fb63d7012ef301b064b33ca6ba48f667ffb685
iamangus <turnbull.angus@gmail.com>
2022-01-19 11:50:40 -0600

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,9 +1,16 @@
 import discord
 import logging
+import argparse
 
 from discord.ext import commands
-
 logging.basicConfig(level=logging.DEBUG)
+
+# Initialize parser
+parser = argparse.ArgumentParser()
+# Adding optional argument
+parser.add_argument("-t", "--bottoken", help = "Discord bot token")
+# Read arguments from command line
+args = parser.parse_args()
 
 client = commands.Bot(command_prefix = '.')
 
@@ -11,4 +18,7 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('Bot is ready.')
 
-client.run('API_KEY')
+client.run('args.bottoken')
+
+ 
+
