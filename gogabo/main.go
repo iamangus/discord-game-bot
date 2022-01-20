59a8fc77de4b033d517aa8ffa61236809c690f3d
iamangus <turnbull.angus@gmail.com>
2022-01-20 16:57:55 -0600

    test

--- gogabo/main.go
+++ gogabo/main.go
@@ -47,6 +47,7 @@ func main() {
 
 	for _, guild := range Session.State.Guilds {
 		log.Printf("guild.ID is " + guild.ID)
+		log.Printf("Channels for guild are: " + Session.GuildChannels(guild.ID))
 	    channels, _ := Session.GuildChannels(guild.ID)
 		for _, c := range channels {
             log.Printf("Channel name is " + c.Name)
